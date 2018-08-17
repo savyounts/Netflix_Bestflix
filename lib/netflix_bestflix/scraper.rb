da55d4804e13c4c5b04ac3479b867ab61bd40e15
Sav Younts <savyounts@gmail.com>
2018-08-17 17:57:54 +0000

    swichted names of Show methods, created #get_page, #scrape_show_index & #make_show in scraper

--- lib/netflix_bestflix/scraper.rb
+++ lib/netflix_bestflix/scraper.rb
@@ -1,3 +1,15 @@
-class Netflix_bestflix::Scraper
 
+class Scraper
+
+  def get_page
+    Nokogiri::HTML(open("https://editorial.rottentomatoes.com/guide/best-netflix-shows-and-movies-to-binge-watch-now"))
+  end
+
+  def scrape_show_index
+    self.get_page.css("div .articleContentBody div")
+  end
+
+  def make_show
+    scrape_show_index.each {|s| Netflix_bestflix::Show.new(r)}
+  end
 end
--- lib/netflix_bestflix/show.rb
+++ lib/netflix_bestflix/show.rb
@@ -2,17 +2,29 @@ class Netflix_bestflix::Show
   attr_accessor :title, :genre, :rt_score, :viewer_score, :cast, :starring, :tv_network, :description, :created_by, :seasons, :rating
   @@all = []
 
-  def initialize(attributes)
-    attributes.each {|k, v| self.send(("#{k}=", v))}
-    @@all << self
+  def initialize(:title = nil, :genre = nil, :rt_score = nil, :viewer_score = nil, :cast = nil, :starring = nil, :tv_network = nil, :description = nil, :created_by = nil, :seasons = nil, :rating = nil)
+    @title = title
+    @genre = genre
+    @rt_score = rt_score
+    @viewer_score = viewer_score
+    @cast = cast
+    @starring = starring
+    @tv_network = tv_network
+    @description = description
+    @created_by = created_by
+    @seasons = seasons
+    @rating = rating
   end
 
-  def self.create_show_from_hash(shows_hash)
-    show.hash.each {|s| new_show = Show.new(s)}
+  def self.new_from_scrape(r)
+    self.new(
+      r.
+
+    )
   end
 
   def self.all
     @@all
-  end 
+  end
 
 end
