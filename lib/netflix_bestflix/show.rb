3cfdacd8ac132ebeb9b707194429e5ca971ac877
Sav Younts <savyounts@gmail.com>
2018-08-17 17:29:05 +0000

    create atrributes, #initiaize, #create_show_from_hash, #all

--- lib/netflix_bestflix/show.rb
+++ lib/netflix_bestflix/show.rb
@@ -1,3 +1,18 @@
 class Netflix_bestflix::Show
+  attr_accessor :title, :genre, :rt_score, :viewer_score, :cast, :starring, :tv_network, :description, :created_by, :seasons, :rating
+  @@all = []
 
-end  
+  def initialize(attributes)
+    attributes.each {|k, v| self.send(("#{k}=", v))}
+    @@all << self
+  end
+
+  def self.create_show_from_hash(shows_hash)
+    show.hash.each {|s| new_show = Show.new(s)}
+  end
+
+  def self.all
+    @@all
+  end 
+
+end
