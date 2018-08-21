26aedc0d037a40b0071d4d6a108e9d7026c758a5
Sav Younts <savyounts@gmail.com>
2018-08-21 16:14:54 +0000

    add all show index pages to scrape -- all 90 shows in show list

--- lib/netflix_bestflix/scraper.rb
+++ lib/netflix_bestflix/scraper.rb
@@ -2,8 +2,23 @@
 class NetflixBestflix::Scraper
 
 
-  def scrape_show_index
+  def scrape_show_index_1
     doc = Nokogiri::HTML(open("https://editorial.rottentomatoes.com/guide/best-netflix-shows-and-movies-to-binge-watch-now/"))
+    doc.css(".countdown-item")
+  end
+
+  def scrape_show_index_2
+    doc = Nokogiri::HTML(open("http://editorial.rottentomatoes.com/guide/best-netflix-shows-and-movies-to-binge-watch-now/2/"))
+    doc.css(".countdown-item")
+  end
+
+  def scrape_show_index_3
+    doc = Nokogiri::HTML(open("http://editorial.rottentomatoes.com/guide/best-netflix-shows-and-movies-to-binge-watch-now/3/"))
+    doc.css(".countdown-item")
+  end
+
+  def scrape_show_index_4
+    doc = Nokogiri::HTML(open("http://editorial.rottentomatoes.com/guide/best-netflix-shows-and-movies-to-binge-watch-now/4/"))
     doc.css(".countdown-item")
   end
 
@@ -13,9 +28,10 @@ class NetflixBestflix::Scraper
   end
 
   def make_shows
-    scrape_show_index.each do |s|
-      NetflixBestflix::Show.new_from_scrape(s)
-    end
+    scrape_show_index_1.each {|s| NetflixBestflix::Show.new_from_scrape(s)}
+    scrape_show_index_2.each {|s| NetflixBestflix::Show.new_from_scrape(s)}
+    scrape_show_index_3.each {|s| NetflixBestflix::Show.new_from_scrape(s)}
+    scrape_show_index_4.each {|s| NetflixBestflix::Show.new_from_scrape(s)}
   end
 
   def make_movies
